f301799ed670364907cf943f2a6298e66565f3af
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-08-25 11:28:05 +0330

    Requesting List of Information and Reviewing Rest Design!

--- core/main.py
+++ core/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+names_list = [
+    {"id":1, "name":"amin"},
+    {"id":2, "name":"nima"},
+    {"id":3, "name":"andreas"},
+    {"id":4, "name":"rastin"},
+    {"id":5, "name":"soheil"},
+]
+
+# /names (GET(RETRIEVE), POST(CREATE))
+
+# /names/:id (GET(RETRIEVE), PUT/PATCH(UPDATE), DELETE)
 
 @app.get("/")
 def root():
     return {"message":"Hello, World!"}
 
-
-# another way of running a fastapi project!
-# import uvicorn
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
-# OR via this command (uvicorn main:app --reload --host 0.0.0.0 --port 8000) in core directory!
-
-# but the suitable one is (fastapi dev core/main.py), just pay attention to fastapi-cli
-#with your module version because it tore me up to fix it! 
+@app.get("/names")
+def retrieve_names_list():
+    return names_list
