305f0850c3ec0f6ca32d10174a60ce7b434a0d0b
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-08-27 13:45:08 +0330

    Using query parameter in filtering the list (search bar)

--- core/main.py
+++ core/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated, Optional
 import random
 
 app = FastAPI()
@@ -9,11 +10,18 @@ names_list = [
     {"id":3, "name":"andreas"},
     {"id":4, "name":"rastin"},
     {"id":5, "name":"soheil"},
+    {"id":6, "name":"amin"},
+    {"id":7, "name":"amin"},
 ]
 
 # /names (GET(RETRIEVE), POST(CREATE))
 @app.get("/names")
-def retrieve_names_list():
+# def retrieve_names_list(q : str | None = None):
+# def retrieve_names_list(q : Optional[str] = None):
+# def retrieve_names_list(q : Annotated[str | None, Query(max_length=50)] = None):
+def retrieve_names_list(q : str | None = Query(default=None, max_length=50)):
+    if q:
+        return [item for item in names_list if item["name"] == q] # [operation iteration condition]        
     return names_list
 
 @app.post("/names")
