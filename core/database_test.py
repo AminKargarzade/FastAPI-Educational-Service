a7b86b2ea538e993c8641befa73b42f4ffd93328
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-09-10 16:57:18 +0330

    Creating model & tables via Base

--- core/database_test.py
+++ core/database_test.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
@@ -10,14 +10,25 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False # only for sqlite
-    }
+    connect_args={"check_same_thread": False},  # only for sqlite
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 # create base class for declaring tables
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    age = Column(Integer)
+
+    def __repre__(self):
+        return f"User(id={self.id}, first_name={self.first_name}, last_name={self.last_name})"
 
 
 # to create tables and database
