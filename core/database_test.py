1ce79f5969c59544b156bdaec7f3483b2437ca7c
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-09-12 01:44:58 +0330

    Doing CRUD stuff in SQLAlchemy!

--- core/database_test.py
+++ core/database_test.py
@@ -24,14 +24,45 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String(30))
-    last_name = Column(String(30))
+    last_name = Column(String(30), nullable=True)
     age = Column(Integer)
     is_active = Column(Boolean, default=True)
     is_verified = Column(Boolean, default=False)
 
-    def __repre__(self):
+    def __repr__(self):
         return f"User(id={self.id}, first_name={self.first_name}, last_name={self.last_name})"
 
 
 # to create tables and database
 Base.metadata.create_all(engine)
+
+session = SessionLocal()
+
+# Inserting Data
+# amin = User(first_name="amin", age="21")
+# session.add(amin)
+# session.commit()
+
+# Bulk Insert
+# andreas = User(first_name="andreas", age="25")
+# rastin = User(first_name="rastin", age="22")
+# users = [andreas, rastin]
+# session.add_all(users)
+# session.commit()
+
+# Retrieve All Data
+# users = session.query(User).all()  # returns all users
+# print(users)
+
+# Retrieve data with filter
+user = session.query(User).filter_by(first_name="amin", age=21).first()
+
+
+# Updating a record of data
+# user.last_name = "Rastin"
+# session.commit()
+
+# Deleting a record of data
+if user:
+    session.delete(user)
+    session.commit()
