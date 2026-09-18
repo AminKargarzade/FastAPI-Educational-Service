b7df1b243e13fec2f711cddaf3b4ed14b0fe42bf
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-09-19 00:05:18 +0330

    Self Referencing

--- core/database_test.py
+++ core/database_test.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from sqlalchemy import (
+    DateTime,
     ForeignKey,
     Text,
     create_engine,
@@ -38,8 +41,11 @@ class User(Base):
     is_verified = Column(Boolean, default=False)
 
     adresses = relationship("Address", backref="user")
-    
-    profile = relationship("Profile", backref="user", uselist=False)  # uselist=False indicates a one-to-one relationship
+    posts = relationship("Post", backref="user")
+    comments = relationship("Comment", backref="user")
+    profile = relationship(
+        "Profile", backref="user", uselist=False
+    )  # uselist=False indicates a one-to-one relationship
 
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}, email={self.email}, is_active={self.is_active}, is_verified={self.is_verified})"
@@ -66,7 +72,7 @@ class Profile(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.id"), unique=True)
-    # Alternative best practice 
+    # Alternative best practice
     # user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)  # This will make user_id the primary key of the Profile table, ensuring a one-to-one relationship with the User table.
 
     first_name = Column(String())
@@ -77,13 +83,61 @@ class Profile(Base):
         return f"Profile(id={self.id}, user_id={self.user_id}, first_name={self.first_name}, last_name={self.last_name}, bio={self.bio})"
 
 
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String())
+    content = Column(Text())
+
+    comments = relationship("Comment", backref="post")
+
+    created_date = Column(DateTime(), default=datetime.now)
+    updated_date = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"Post(id={self.id}, user_id={self.user_id}, title={self.title}, content={self.content})"
+
+
+from sqlalchemy.orm import backref
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    parent_id = Column(
+        Integer, ForeignKey("comments.id"), nullable=True
+    )  # self-referential foreign key for nested comments
+
+    # parent = relationship(
+    #     "Comment", back_populates="children", remote_side=[id]
+    # )  # relationship for nested comments
+    children = relationship(
+        "Comment", backref=backref("parent", remote_side=[id])
+    )  # relationship for nested comments
+
+    content = Column(Text())
+
+    created_date = Column(DateTime(), default=datetime.now)
+    updated_date = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"Comment(id={self.id}, post_id={self.post_id}, user_id={self.user_id}, parent_id={self.parent_id}, content={self.content})"
+
+
 # to create tables and database
 Base.metadata.create_all(engine)
 
 session = SessionLocal()
 
 user = session.query(User).filter_by(username="AminRastin").one_or_none()
+post = user.posts[0]
 
-# session.add(Profile(user_id=user.id, first_name="Amin", last_name="Rastin", bio="Software Engineer"))
-# session.commit()
-print(user.profile.first_name) # This will print the profile associated with the user, if it exists.
+comments = session.query(Comment).filter_by(post_id=post.id, parent_id=None).all()
+
+for comment in comments:
+    print(comment.children)
